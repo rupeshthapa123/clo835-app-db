a7e7098f2ff423a4ee1e7a0ce6997eb383bde303
Rupesh Thapa <rupesh.thapa2050@gmail.com>
2025-06-26 21:30:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import argparse
 
 
 app = Flask(__name__)
-
+print("All ENV vars:", os.environ)
 DBHOST = os.environ.get("DBHOST") or "localhost"
 DBUSER = os.environ.get("DBUSER") or "root"
 DBPWD = os.environ.get("DBPWD") or "passwors"
